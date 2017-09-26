4583a9f85d0734d5d027e2644aa8c04e4d111bc4
idlocalhost <32290514+idlocalhost@users.noreply.github.com>
2017-09-26 18:37:25 +0700

    Update custom-multy.js

--- custom-multy.js
+++ custom-multy.js
@@ -26,7 +26,7 @@ if(x=="2"){xxx="09/7-reasons-to-trade-forex.html"}
 if(x=="3"){xxx="09/bad-habits-in-your-forex-trading.html"}
 if(x=="4"){xxx="09/do-you-forecast-forex-or-use-reality.html"}
 if(x=="5"){xxx="09/forex-and-twitter-two-growing-forces.html"}
-if(x=="5"){xxx="09/forex-liquidity.html"}a_to_ve[i].href="https://soholink.blogspot.com/2017/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="6"){xxx="09/forex-liquidity.html"}a_to_ve[i].href="https://soholink.blogspot.com/2017/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
